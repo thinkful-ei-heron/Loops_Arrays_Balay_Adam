05ab7c3a678ae298c18f0924fe8f6b6dd2aa0b72
Adam Newhouser <adamnewhouser@gmail.com>
2019-09-17 14:30:55 -0400

    filternames

--- scripts.js
+++ scripts.js
@@ -53,9 +53,33 @@ function goodbye() {
   console.log('Goodbye world');
 }
 
-console.log(repeat(hello, 5));
-console.log(repeat(goodbye, 5));
+// console.log(repeat(hello, 5));
+// console.log(repeat(goodbye, 5));
 
+
+function filter(arr, fn) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i]) === true) {
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
+}
+
+// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// First we setup an array of strings we plan to filter:
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+// We use your `filter` function here, capturing a new array into `filteredNames`
+// comprised of names that only begin with 'R'
+const filteredNames = filter(myNames, (name) => name[0] === 'R');
+  // This is known as a "predicate function" - it's a function that 
+  // only returns a boolean
+
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
 
 
 
